e55c80b10c29a7dd8bb4dba8d09385ab071ad4dd
mitragroups <miftahmuhajir@gmail.com>
2024-05-05 04:53:45 +0700

    Try for render and redirect_to in Controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,4 +5,12 @@ class HomeController < ApplicationController
   def iseng
     render plain: 'Iseng - iseng aja nih'
   end
+
+  def page
+    render 'landing'
+  end
+
+  def keren
+    render 'landing'
+  end
 end
