e3e72919fcbf80f130717377761a88c78d2f1dad
mitragroups <miftahmuhajir@gmail.com>
2024-05-05 17:55:01 +0700

    Fixing limit for integer, integer can't limit

--- db/migrate/20240505104602_create_books.rb
+++ db/migrate/20240505104602_create_books.rb
@@ -1,8 +1,8 @@
 class CreateBooks < ActiveRecord::Migration[7.1]
   def change
     create_table :books do |t|
-      t.string :title, :default => 'Belum ada judul'
-      t.integer :page, null: false, limit: 100
+      t.string :title, :default => 'Belum ada judul', limit: 100
+      t.integer :page, null: false
       t.integer :price
       t.text :description, default: 'Silahkan isi deskripsi buku nya'
 
