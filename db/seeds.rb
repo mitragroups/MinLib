29e1689c3ebacba43db7ed6bc79ffd48662ca377
mitragroups <miftahmuhajir@gmail.com>
2024-05-05 18:18:24 +0700

    Add Book#Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Book.create(title: 'Flutter', page: 350, price: 350_000, description: 'Bagus untuk mempelajari cross platform')
+Book.create(title: 'Java Springboot', price: 620_000, page: 620, description: 'Development web base cocok untuk banking')
+Book.create(title: 'Go-Lang', page: 75, price: 75_000, description: 'Bahasa pemrograman dari Google')
+Book.create(title: 'Laravel', page: 200, price: 200_000, description: 'Framework PHP')
+Book.create(title: 'Python', page: 100, price: 100_000, description: 'Cocok untuk Machine Learning dan Data Science')
