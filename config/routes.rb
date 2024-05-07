7f2c7c2e1db07da0f286316b8c2d10de8ebf836f
mitragroups <miftahmuhajir@gmail.com>
2024-05-07 20:45:30 +0700

    Add resources :books at routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   get 'home/iseng'
   get 'home/page'
   get 'home/keren'
+
+  resources :books
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
