fd71193b73cc5c6292588c86d202ba3475e87342
mitragroups <miftahmuhajir@gmail.com>
2024-05-20 10:21:37 +0700

    Add method male_authors_oldest

--- app/models/author.rb
+++ app/models/author.rb
@@ -4,10 +4,16 @@ class Author < ApplicationRecord
   def self.male_author
     where("gender != 'Female'")
   end
+
   def self.oldest
     order(age: :desc)
   end
+
   def self.youngest
     order(age: :asc).pluck(:name, :age)
+  end
+
+  def self.male_author_oldest
+    male_author.oldest
   end
 end
